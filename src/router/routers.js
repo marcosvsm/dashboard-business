6e04d2b76824cc575d2149c84a9fc7610ad23bc7
Marcos <marcosvsmendes@gmail.com>
2024-03-31 10:56:38 +1100

    fixed path

--- src/router/routers.js
+++ src/router/routers.js
@@ -3,18 +3,18 @@ import auth from "@/middleware/auth"
 import guest from "@/middleware/guest"
 
 //Pages
-import Login from '../components/views/pages/Login.vue'
-import Register from '../components/views/pages/Register.vue'
+import Login from '@/components/views/pages/Login.vue'
+import Register from '@/components/views/pages/Register.vue'
 
 //Dashboard
-import Overview from '../components/views/pages/dashboard/overview/Overview.vue'
+import Overview from '@/components/views/pages/dashboard/overview/Overview.vue'
 
 //Invoice
-import InvoiceAdd from '../components/views/pages/invoice/InvoiceAdd.vue'
-import InvoiceFree from '../components/views/pages/invoice/InvoiceAdd.vue'
+import InvoiceAdd from '@/components/views/pages/invoice/InvoiceAdd.vue'
+import InvoiceFree from '@/components/views/pages/invoice/InvoiceAdd.vue'
 
-import Company from '../components/views/pages/Company.vue'
-import CompanyList from '../components/views/pages/company/CompanyList.vue'
+import Company from '@/components/views/pages/Company.vue'
+import CompanyList from '@/components/views/pages/company/CompanyList.vue'
 
 let loginPage = {
     path: '/login',
